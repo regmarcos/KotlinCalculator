0d38e0a97d2213eb61d0b771be90a1cbbc088135
regmarcos <marcos.reginato@globant.com>
2019-11-25 16:25:02 -0300

    change the CalculatorModel as a Data class

--- app/src/main/java/com/example/kotlincalculator/mvp/model/CalculatorModel.kt
+++ app/src/main/java/com/example/kotlincalculator/mvp/model/CalculatorModel.kt
@@ -2,11 +2,7 @@ package com.example.kotlincalculator.mvp.model
 
 import com.example.kotlincalculator.utils.EMPTY_STRING
 
-class CalculatorModel {
-    var firstOperand : String = EMPTY_STRING
-    var secondOperand : String = EMPTY_STRING
-    var operator : String = EMPTY_STRING
-    var result : String = EMPTY_STRING
+class CalculatorModel (var firstOperand : String = EMPTY_STRING, var secondOperand : String = EMPTY_STRING, var operator : String = EMPTY_STRING, var result : String = EMPTY_STRING) {
     fun cleanVisor(){
         firstOperand = EMPTY_STRING
         secondOperand = EMPTY_STRING
